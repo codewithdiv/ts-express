dc0d08dd0ef9fd3e118186da7f4678511f724eb7
Divinewill ZERO <zerothread@users.noreply.github.com>
2022-01-21 16:29:33 +0100

    [ Bug 🐞 ] Fixed typo && implemeneted approuter middleware

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
-import express, { Request, Response, NextFunction } from "express";
-import { router } from "./routes/loginRoute";
-import bodyParser from "body-parser";
-import cookieSession from "cookie-session";
+import express, { Request, Response, NextFunction } from 'express';
+import { router } from './routes/loginRoute';
+import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
+import './controller/LoginController';
+import { AppRouter } from './AppRouter';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieSession({ keys: ["testing"] }));
+app.use(cookieSession({ keys: ['testing'] }));
 app.use(router);
+app.use(AppRouter.getInstance());
 
 app.listen(8080, () => {
   console.log(`App is running on port 8080`);
